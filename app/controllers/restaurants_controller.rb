b599e6a654306dd3a1765512790d64ff6022b5ba
Jules Prevost <jprevostdev@gmail.com>
2020-06-10 12:53:33 +0200

    all routes done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [ :show ]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(task_params)
+    if @restaurant.valid?
+      @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy all restaurants in the db"
+Restaurant.destroy_all
+puts "Create restaurants in the db"
+Restaurant.create(name: "Chez Jules", address: "11 rue des Prévost", phone_number: "+33 6 35 55 39 98", category: "french")
+Restaurant.create(name: "Fabrice Place", address: "11 rue des Fabrice", phone_number: "+33 6 34 55 39 98", category: "italian")
+Restaurant.create(name: "McDo", address: "Place de la Bastille", phone_number: "+44 6 35 55 39 98", category: "belgian")
+Restaurant.create(name: "SushiShop", address: "44 rue des Makis", phone_number: "+33 6 35 99 39 98", category: "japanese")
+Restaurant.create(name: "Chinese story", address: "Square des Restos", phone_number: "+33 6 35 55 88 98", category: "chinese")
+
+puts "Finish"
