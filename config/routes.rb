1b4091c5ecc05d93d8c7747bae1a07e3f0eda7f1
Jules Prevost <jprevostdev@gmail.com>
2020-06-10 13:02:48 +0200

    add link from show to index and link from show to create a review

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
   resources :restaurants, only: [ :index, :show, :new, :create ] do
     resources :reviews, only: [ :new, :create ]
   end
-
-  #faire resources reviews
-
 end
